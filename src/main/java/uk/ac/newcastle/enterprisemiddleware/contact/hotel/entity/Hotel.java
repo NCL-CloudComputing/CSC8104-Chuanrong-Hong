ab8c7e68cffb2a903f073fcdb0551e02e9be9ac9
hcc <hcc@163.cn>
2023-11-22 21:31:43 +0000

    hotel1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Hotel.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Hotel.java
@@ -1,4 +1,22 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@Entity
+@XmlRootElement
+@Table(name = "hotel",uniqueConstraints = @UniqueConstraint(columnNames = "phone_number"))
 public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+
+    @NotNull
+    @Size(min=1,max = 50)
+    @Pattern(regexp ="^[A-Za-z]+$",message = "Hotel name must only contain letters")
+    private String hotelName;
 }
