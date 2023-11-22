2a2e26e49c10d5fa5c2d78116caa262611f8059d
hcc <hcc@163.cn>
2023-11-22 21:54:51 +0000

    hotel2

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Hotel.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Hotel.java
@@ -1,6 +1,7 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -18,5 +19,49 @@ public class Hotel {
     @NotNull
     @Size(min=1,max = 50)
     @Pattern(regexp ="^[A-Za-z]+$",message = "Hotel name must only contain letters")
+    @Column(name = "name")
     private String hotelName;
+
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z0-9]{6}$", message = "Please input a correct zip code")
+    private String zipCode;
+
+    @NotNull
+    @Pattern(regexp = "^\\([2-9][0-8][0-9]\\)\\s?[0-9]{3}\\-[0-9]{4}$",message = "please input correct phone number")
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
