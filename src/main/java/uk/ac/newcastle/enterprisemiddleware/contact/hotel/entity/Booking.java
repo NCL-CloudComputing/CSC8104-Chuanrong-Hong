6de81b7f5406bb3535cb2ef3899b252bbdf321fa
hcc <hcc@163.cn>
2023-11-23 05:01:59 +0000

    booking1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Booking.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Booking.java
@@ -1,4 +1,16 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity;
 
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@NamedQueries({
+})
+@XmlRootElement
+@Table(name = "booking", uniqueConstraints = @UniqueConstraint(columnNames = {"hotel_id" , "booking_time"}))
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
 }
