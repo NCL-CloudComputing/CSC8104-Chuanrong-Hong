df849fe8f3e7c4bbb6a9d49bd5db6e0f5df4d9e5
hcc <hcc@163.cn>
2023-11-23 04:49:59 +0000

    customer1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Customer.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Customer.java
@@ -1,4 +1,25 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@NamedQueries({
+})
+@XmlRootElement
+@Table(name = "customer", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+
+    @NotNull
+    @Size(min=1,max = 50)
+    @Pattern(regexp ="^[A-Za-z]+$",message = "Please use a name without numbers or specials")
+    @Column(name = "name")
+    private String name;
+
 }
