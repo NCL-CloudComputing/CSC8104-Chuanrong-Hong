589392757d9d7266bd1166a079abd7e283e226ed
hcc <hcc@163.cn>
2023-11-23 04:56:19 +0000

    customer2

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Customer.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/entity/Customer.java
@@ -22,4 +22,55 @@ public class Customer {
     @Column(name = "name")
     private String name;
 
+    @NotNull
+    @NotEmpty
+    @Email(message = "The email address must be in the format of name@domain.com")
+    private String email;
+
+    @NotNull
+    @Pattern(regexp = "^\\([2-9][0-8][0-9]\\)\\s?[0-9]{3}\\-[0-9]{4}$",message = "please input correct number")
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
