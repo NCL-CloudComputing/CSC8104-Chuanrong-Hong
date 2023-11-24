174c00e09ef26841f2aa58366a72c7266aad3d0e
hcc <hcc@163.cn>
2023-11-24 05:36:20 +0000

    createhotel

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/GuestBookingRestService.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/GuestBookingRestService.java
@@ -104,8 +104,61 @@ import java.util.logging.Logger;
             return builder.entity(new ServiceReturn(customerService.getAllCustomers(),true)).build();
         }
 
+        @Path("/hotelCreate")
+        @POST
+        @Operation(description = "Add a new Hotel to the database")
+        @APIResponses(value = {
+                @APIResponse(responseCode = "201", description = "Hotel created successfully."),
+                @APIResponse(responseCode = "400", description = "Invalid Hotel supplied in request body"),
+                @APIResponse(responseCode = "409", description = "Hotel supplied in request body conflicts with an existing Hotel"),
+                @APIResponse(responseCode = "500", description = "An unexpected error occurred whilst processing the request")
+        })
+        public Response createCustom(Hotel hotel){
+            Response.ResponseBuilder builder = Response.status(Response.Status.CREATED).entity(new ServiceReturn("success!" , true));
+            if (hotel == null) {
+                return Response.status(Response.Status.BAD_REQUEST).entity(new ServiceReturn("request body is null" , false)).build();
+            }
+
+            try{
+                transaction.begin();
+                hotelService.createHotel(hotel);
+                transaction.commit();
+            }catch (Exception e){
+                builder = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ServiceReturn(e.getMessage() , false));
+                log.info("commit error ----->" + hotel.toString());
+                log.info("cause ----->" + e.getMessage() + ":" + e.getCause());
+                if (e.getCause() instanceof ConstraintViolationException || e.getCause() instanceof PersistenceException){
+                    Set<ConstraintViolation<?>> violations = new LinkedHashSet<>();
+                    StringBuffer sb = new StringBuffer();
+                    if(e.getCause() instanceof ConstraintViolationException){
+                        violations = ((ConstraintViolationException)e.getCause()).getConstraintViolations();
+                    }
+                    if(e.getCause() instanceof PersistenceException){
+                        sb.append("already have same record");
+                    }
+                    for (ConstraintViolation<?> violation : violations) {
+                        String errorMessage = violation.getMessage();
+                        sb.append(violation.getPropertyPath());
+                        sb.append(":");
+                        sb.append(errorMessage);
+                        sb.append("|");
+                    }
+                    sb.substring(0,sb.length()-1);
+                    builder = Response.status(Response.Status.CONFLICT).entity(new ServiceReturn(sb.toString() , false));
+                }
+                try {
+                    transaction.rollback();
+                } catch (Exception ex) {
+                    log.info("rollback error ----->" + ex.getMessage() + ":" + e.getCause());
+                }
+            }
+
+            return builder.build();
+        }
 
 
 
 
-}
+
+
+    }
