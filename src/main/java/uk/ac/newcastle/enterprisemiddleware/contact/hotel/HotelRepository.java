11b03e0249c8234049ca08150d3999ef1012ba44
hcc <hcc@163.cn>
2023-11-24 05:47:43 +0000

    repository1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/HotelRepository.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/HotelRepository.java
@@ -1,5 +1,36 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel;
 
+import uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity.Hotel;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RequestScoped
 public class HotelRepository {
+    @Inject
+    @Named("logger")
+    Logger log;
+
+    @Inject
+    EntityManager em;
+
+    void create(Hotel hotel) {
+        em.persist(hotel);
+    }
+
+    List<Hotel> getHotel(){
+        Query query = em.createQuery("select c from Hotel c");
+        return query.getResultList();
+    }
+
+    Boolean checkHotel(long id){
+        Query query = em.createQuery("select c from Hotel c where id = "+ id);
+        return query.getResultList().size() > 0 ? true : false;
+    }
 
 }
