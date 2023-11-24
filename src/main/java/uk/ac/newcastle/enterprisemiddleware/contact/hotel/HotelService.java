2bc1fe601189d96f7666dbc1c5da71b25e4ee53e
hcc <hcc@163.cn>
2023-11-24 05:46:54 +0000

    hotel1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/HotelService.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/HotelService.java
@@ -1,4 +1,20 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel;
 
+import uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity.Hotel;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import java.util.List;
+
+@Dependent
 public class HotelService {
+    @Inject
+    HotelRepository hotelRepository;
+
+    void createHotel(Hotel hotel){ hotelRepository.create(hotel); }
+
+
+    List<Hotel> getAllHotels(){ return hotelRepository.getHotel();}
+
+    Boolean checkHotel(long id){ return hotelRepository.checkHotel(id);}
 }
