1bcd4f7761a06413d6fb167cb70897485b167c02
hcc <hcc@163.cn>
2023-11-24 05:55:08 +0000

    BookingRepository1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/BookingRepository.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/BookingRepository.java
@@ -1,4 +1,36 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel;
 
+import uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity.Booking;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RequestScoped
 public class BookingRepository {
+    @Inject
+    @Named("logger")
+    Logger log;
+
+    @Inject
+    EntityManager em;
+
+    void create(Booking booking) {
+        em.persist(booking);
+    }
+
+    List<Booking> getAllBookingById(long id){
+        Query query = em.createQuery("select c from Booking c where customer_id = "+ id);
+        return query.getResultList();
+    }
+
+    Boolean delBooking(long id){
+        Query query = em.createQuery("delete from Booking where id = "+ id);
+        return query.executeUpdate() == 1;
+    }
 }
+
