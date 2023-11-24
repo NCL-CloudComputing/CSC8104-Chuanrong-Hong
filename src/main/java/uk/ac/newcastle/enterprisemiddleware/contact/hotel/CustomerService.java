d360dc98256a9eb0fb3cf7b0b6910532c4da5d47
hcc <hcc@163.cn>
2023-11-24 05:44:26 +0000

    customer1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/CustomerService.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/CustomerService.java
@@ -1,4 +1,20 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel;
 
+import uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity.Customer;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import java.util.List;
+
+@Dependent
 public class CustomerService {
+    @Inject
+    CustomerRepository customerRepository;
+
+    void createCustom(Customer customer){ customerRepository.create(customer); }
+
+
+    List<Customer> getAllCustomers(){ return customerRepository.getAllCustomers();}
+
+    Boolean checkCustomer(long id){ return customerRepository.checkCustomer(id);}
 }
