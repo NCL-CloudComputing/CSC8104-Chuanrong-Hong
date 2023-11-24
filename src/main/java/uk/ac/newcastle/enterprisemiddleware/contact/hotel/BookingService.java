53273d6bcc8421340e0e3e778d71bd0e96b5d40e
hcc <hcc@163.cn>
2023-11-24 05:54:24 +0000

    bookingservice1

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/BookingService.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/hotel/BookingService.java
@@ -1,4 +1,19 @@
 package uk.ac.newcastle.enterprisemiddleware.contact.hotel;
 
+import uk.ac.newcastle.enterprisemiddleware.contact.hotel.entity.Booking;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import java.util.List;
+
+@Dependent
 public class BookingService {
+    @Inject
+    BookingRepository bookingRepository;
+
+    void createBooking(Booking booking){ bookingRepository.create(booking); }
+
+    List<Booking> getAllBookingById(long id){ return bookingRepository.getAllBookingById(id);}
+
+    Boolean delBooking(long id){return bookingRepository.delBooking(id);}
 }
